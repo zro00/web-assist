3c8fc91e23fb48b20ea302d880084f8f2c9775f3
rajvir.jhawar@gmail.com <rajvir.jhawar@gmail.com>
2020-08-31 04:22:52 -0600

    Enabled websocket added wake word engine

--- static/index.js
+++ static/index.js
@@ -11,7 +11,7 @@ const directLine = new DirectLine({
   secret: "qOk-KiMu8IU.M4SODPphbB_HY8EmgS65hrPJ4VBSQTb1FxN5x3nCqV0" /* put your Direct Line secret here */,
   //token: /* or put your Direct Line token here (supply secret OR token, not both) */,
   //domain: /* optional: if you are not using the default Direct Line endpoint, e.g. if you are using a region-specific endpoint, put its full URL here */
-  webSocket: false /* optional: false if you want to use polling GET to receive messages. Defaults to true (use WebSocket). */,
+  webSocket: true /* optional: false if you want to use polling GET to receive messages. Defaults to true (use WebSocket). */,
   //pollingInterval: /* optional: set polling interval in milliseconds. Defaults to 1000 */,
   //timeout: /* optional: a timeout in milliseconds for requests to the bot. Defaults to 20000 */,
   conversationStartProperties: { /* optional: properties to send to the bot on conversation start */

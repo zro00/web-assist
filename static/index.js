a2b58a8d6472da9d4f4d0894b82ebdb96a32e5dc
rajvir.jhawar@gmail.com <rajvir.jhawar@gmail.com>
2020-09-03 18:45:08 -0600

    Added security for websocket

--- static/index.js
+++ static/index.js
@@ -5,7 +5,7 @@ if ("WebSocket" in window) {
     alert("WebSocket is supported by your Browser!");
     
     // Let us open a web socket
-    var ws = new WebSocket("ws://34.122.187.227:8000");
+    var ws = new WebSocket("wss://34.122.187.227:8000");
     test();
 } else {
   

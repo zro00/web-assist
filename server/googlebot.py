9b81ae712a82d14865c85be6d5067582c6f04d3b
rajvir.jhawar@gmail.com <rajvir.jhawar@gmail.com>
2020-09-05 19:48:51 -0600

    Fixed indent

--- server/googlebot.py
+++ server/googlebot.py
@@ -49,27 +49,27 @@ def detect_intent_with_texttospeech_response(project_id, session_id, client_msg,
     print(f"the TEXT INPUT {text_input}")
     query_input = dialogflow.types.QueryInput(text=text_input)
 
-        # Set the query parameters with sentiment analysis
-        output_audio_config = dialogflow.types.OutputAudioConfig(
-            audio_encoding=dialogflow.enums.OutputAudioEncoding
-            .OUTPUT_AUDIO_ENCODING_LINEAR_16)
+    # Set the query parameters with sentiment analysis
+    output_audio_config = dialogflow.types.OutputAudioConfig(
+        audio_encoding=dialogflow.enums.OutputAudioEncoding
+        .OUTPUT_AUDIO_ENCODING_LINEAR_16)
 
-        response = session_client.detect_intent(
-            session=session_path, query_input=query_input,
-            output_audio_config=output_audio_config)
+    response = session_client.detect_intent(
+        session=session_path, query_input=query_input,
+        output_audio_config=output_audio_config)
 
-        print('=' * 20)
-        print('Query text: {}'.format(response.query_result.query_text))
-        print('Detected intent: {} (confidence: {})\n'.format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence))
-        print('Fulfillment text: {}\n'.format(
-            response.query_result.fulfillment_text))
-        return response.output_audio
-        # The response's audio_content is binary.
-        #with open('output.wav', 'wb') as out:
-            #out.write(response.output_audio)
-            #print('Audio content written to file "output.wav"')
+    print('=' * 20)
+    print('Query text: {}'.format(response.query_result.query_text))
+    print('Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence))
+    print('Fulfillment text: {}\n'.format(
+        response.query_result.fulfillment_text))
+    return response.output_audio
+    # The response's audio_content is binary.
+    #with open('output.wav', 'wb') as out:
+        #out.write(response.output_audio)
+        #print('Audio content written to file "output.wav"')
 
 
 @app.websocket_route('/voice')
